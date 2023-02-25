2376890617263dbea34308f6272443146fb3eb13
CHooge <37962713+hyperiongeo@users.noreply.github.com>
2023-02-24 17:17:15 -0700

    minor adjustments

--- ribbon/MainWindow.py
+++ ribbon/MainWindow.py
@@ -14,89 +14,10 @@ from ribbon.RibbonWidget import RibbonWidget, RibbonTabBar
 __author__ = 'mamj'
 
 
-# class MainWindowwithRibbonOLD(QMainWindow):
-#     def __init__(self, ui_file=None, parent=None):
-#         if ui_file is not None:
-#             super().__init__(parent)
-#             loadUi(ui_file, self)
-#             self.resize(1280, 800)
-#         else:
-#             super().__init__(None)
-
-#             self.resize(1280, 800)
-#             self.setWindowTitle("Main Window")
-#             # self.setDockNestingEnabled(True)
-#             # self.setWindowIcon(get_icon("icon"))
-#             # self._main_dock_widget = QDockWidget(self)
-#             # self._main_dock_widget.setObjectName("MainDock")
-#             # self._main_dock_widget.setWindowTitle("Main dock")
-#             # self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self._main_dock_widget)
-#             self.centralWidget()
-
-#         # -------------      actions       -----------------
-
-#         # self._open_action = self.add_action("Open", "open", "Open file", True, self.on_open_file, QKSec.StandardKey.Open)
-#         # self._save_action = self.add_action("Save", "save", "Save file", True, self.on_save, QKSec.StandardKey.Save)
-#         # self._copy_action = self.add_action("Copy", "copy", "Copy selection", True, self.on_copy, QKSec.StandardKey.Copy)
-#         # self._paste_action = self.add_action("Paste", "paste", "Paste from clipboard", True, self.on_paste, QKSec.StandardKey.Paste)
-#         # self._zoom_action = self.add_action("Zoom", "zoom", "Zoom in on document", True, self.on_zoom)
-#         # self._about_action = self.add_action("About", "about", "About QupyRibbon", True, self.on_about)
-#         # self._license_action = self.add_action("License", "license", "Licence for this software", True, self.on_license)
-
-#         # -------------      textboxes       -----------------
-
-#         # self._text_box1 = RibbonTextbox("Text 1", self.on_text_box1_changed, 80)
-#         # self._text_box2 = RibbonTextbox("Text 2", self.on_text_box1_changed, 80)
-#         # self._text_box3 = RibbonTextbox("Text 3", self.on_text_box1_changed, 80)
-
-#         # Ribbon
-#         self.show()
-
-#         self._ribbon = RibbonWidget(self)
-#         self.addToolBar(self._ribbon)
-#         self.init_ribbon()
-
-#     def add_action(self, caption, icon_name, status_tip, icon_visible, connection, shortcut=None):
-#         action = QAction(get_icon(icon_name), caption, self)
-#         action.setStatusTip(status_tip)
-#         action.triggered.connect(connection)
-#         action.setIconVisibleInMenu(icon_visible)
-#         if shortcut is not None:
-#             action.setShortcuts(shortcut)
-#         self.addAction(action)
-#         return action
-
-#     def init_ribbon(self):
-#         home_tab = self._ribbon.add_ribbon_tab("Project")
-#         file_pane = home_tab.add_ribbon_pane("File")
-
-#         file_pane.add_ribbon_widget(RibbonButton(self, self._open_action, True))
-#         file_pane.add_ribbon_widget(RibbonButton(self, self._save_action, True))
-
-#         edit_panel = home_tab.add_ribbon_pane("Edit")
-#         edit_panel.add_ribbon_widget(RibbonButton(self, self._copy_action, True))
-#         edit_panel.add_ribbon_widget(RibbonButton(self, self._paste_action, True))
-#         grid = edit_panel.add_grid_widget(200)
-#         grid.addWidget(QLabel("Text box 1"), 1, 1)
-#         grid.addWidget(QLabel("Text box 2"), 2, 1)
-#         grid.addWidget(QLabel("Text box 3"), 3, 1)
-#         grid.addWidget(self._text_box1, 1, 2)
-#         grid.addWidget(self._text_box2, 2, 2)
-#         grid.addWidget(self._text_box3, 3, 2)
-
-#         view_panel = home_tab.add_ribbon_pane("View")
-#         view_panel.add_ribbon_widget(RibbonButton(self, self._zoom_action, True))
-#         home_tab.add_spacer()
-
-#         about_tab = self._ribbon.add_ribbon_tab("About")
-#         info_panel = about_tab.add_ribbon_pane("Info")
-#         info_panel.add_ribbon_widget(RibbonButton(self, self._about_action, True))
-#         info_panel.add_ribbon_widget(RibbonButton(self, self._license_action, False))
-
 
 class RibbonMainWindow(QMainWindow):
     def __init__(self, parent=None, dock=False, title="Prospector"):
-        super().__init__(None)
+        super().__init__(parent)
         self.resize(1280, 800)
         self.setWindowTitle(title)
         self.setDockNestingEnabled(True)
--- ribbon/RibbonCombobox.py
+++ ribbon/RibbonCombobox.py
@@ -5,14 +5,15 @@ from pyqtgraph.Qt.QtWidgets import QComboBox
 from pyqtgraph.Qt.QtGui import QFont
 
 class RibbonCombobox(QComboBox):
-    def __init__(self, default_value, change_connector, max_width=50, label=None):
+    def __init__(self, default_value, change_connector, min_width=50, max_width=50, label=None, font_size=9):
         super().__init__()
         font = QFont()
-        font.setPointSize(9)
+        font.setPointSize(font_size)
         self.setFont(font)
 
         self.setStyleSheet("border: 1px solid rgba(0,0,0,30%);")
         self.setCurrentText(default_value)
-        self.setMaximumWidth(max_width)
+        self.setMinimumWidth(min_width)
+        # self.setMaximumWidth(max_width)
         if change_connector is not None:
             self.currentTextChanged.connect(change_connector)
