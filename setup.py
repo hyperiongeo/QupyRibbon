920458049f73642b71f5a8424c282b4d56ab084c
CHooge <37962713+hyperiongeo@users.noreply.github.com>
2023-03-29 12:34:35 -0600

    more setup updates

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='qupyribbon',
+setup(name='QupyRibbon',
       version='0.1',
-      description='qupyribbon ribbon code',
+      description='QupyRibbon ribbon code',
       url='https://github.com/hyperiongeo/qupyribbon',
       author='Hyperion GeoServices',
       author_email='hyperion.geo@gmail.com',
       license='MIT',
-      packages=['qupyribbon'],
+      packages=['ribbon', 'icons', 'stylesheets'],
       zip_safe=False)
 	  
